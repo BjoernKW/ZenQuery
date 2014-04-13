75b62beb27d0cf21d62b92afa5942f545c91fb3e
Björn Wilmsmann <bjoern@wilmsmann.de>
2014-04-13 16:01:52 +0200

    JSON API

---
+++ src/main/java/com/zenquery/model/dao/DatabaseConnectionDAO.java
@@ -0,0 +1,20 @@
+package com.zenquery.model.dao;
+
+import com.zenquery.model.DatabaseConnection;
+
+import java.util.List;
+
+/**
+ * Created by willy on 13.04.14.
+ */
+public interface DatabaseConnectionDAO {
+    public DatabaseConnection find(Integer id);
+
+    public List<DatabaseConnection> findAll();
+
+    public void insert(DatabaseConnection databaseConnection);
+
+    public void update(Integer id, DatabaseConnection databaseConnection);
+
+    public void delete(Integer id);
+}
--- src/main/java/com/zenquery/model/dao/impl/JdbcDatabaseConnectionDAO.java
+++ src/main/java/com/zenquery/model/dao/impl/JdbcDatabaseConnectionDAO.java
@@ -1,18 +1,19 @@
 package com.zenquery.model.dao.impl;
 
 import com.zenquery.model.DatabaseConnection;
-import com.zenquery.model.dao.DatabaseConnectioDAO;
+import com.zenquery.model.dao.DatabaseConnectionDAO;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.simple.ParameterizedRowMapper;
 
 import javax.sql.DataSource;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.List;
 
 /**
  * Created by willy on 13.04.14.
  */
-public class JdbcDatabaseConnectionDAO implements DatabaseConnectioDAO {
+public class JdbcDatabaseConnectionDAO implements DatabaseConnectionDAO {
     private DataSource dataSource;
     private JdbcTemplate jdbcTemplate;
 
@@ -20,8 +21,8 @@ public class JdbcDatabaseConnectionDAO implements DatabaseConnectioDAO {
         this.dataSource = dataSource;
     }
 
-    public DatabaseConnection find(Integer id){
-        String sql = "SELECT * FROM database_connections WHERE id=?";
+    public DatabaseConnection find(Integer id) {
+        String sql = "SELECT * FROM database_connections WHERE id = ?";
 
         jdbcTemplate = new JdbcTemplate(dataSource);
 
@@ -29,6 +30,52 @@ public class JdbcDatabaseConnectionDAO implements DatabaseConnectioDAO {
                 jdbcTemplate.query(sql, new Object[] { id }, new DatabaseConnectionMapper()).get(0);
 
         return databaseConnection;
+    }
+
+    public List<DatabaseConnection> findAll() {
+        String sql = "SELECT * FROM database_connections";
+
+        jdbcTemplate = new JdbcTemplate(dataSource);
+
+        List<DatabaseConnection> databaseConnections =
+                jdbcTemplate.query(sql, new DatabaseConnectionMapper());
+
+        return databaseConnections;
+    }
+
+    public void insert(DatabaseConnection databaseConnection) {
+        String sql = "INSERT INTO database_connections (name, url, username, password) VALUES (?, ?, ?, ?)";
+
+        jdbcTemplate = new JdbcTemplate(dataSource);
+
+        jdbcTemplate.update(sql, new Object[] {
+                databaseConnection.getName(),
+                databaseConnection.getUrl(),
+                databaseConnection.getUsername(),
+                databaseConnection.getPassword()
+        });
+    }
+
+    public void update(Integer id, DatabaseConnection databaseConnection) {
+        String sql = "UPDATE database_connections SET name = ?, url = ?, username = ?, password = ? WHERE id = ?";
+
+        jdbcTemplate = new JdbcTemplate(dataSource);
+
+        jdbcTemplate.update(sql, new Object[]{
+                databaseConnection.getName(),
+                databaseConnection.getUrl(),
+                databaseConnection.getUsername(),
+                databaseConnection.getPassword(),
+                id
+        });
+    }
+
+    public void delete(Integer id) {
+        String sql = "DELETE FROM database_connections WHERE id = ?";
+
+        jdbcTemplate = new JdbcTemplate(dataSource);
+
+        jdbcTemplate.update(sql);
     }
 
     private static class DatabaseConnectionMapper implements ParameterizedRowMapper<DatabaseConnection> {
