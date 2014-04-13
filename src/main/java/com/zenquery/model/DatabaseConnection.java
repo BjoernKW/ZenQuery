fe7d0fe200a9635752156d9ef6f1cce51a2c0ed5
Björn Wilmsmann <bjoern@wilmsmann.de>
2014-04-13 13:01:00 +0200

    Database design

---
+++ src/main/java/com/zenquery/model/DatabaseConnection.java
@@ -0,0 +1,54 @@
+package com.zenquery.model;
+
+import java.io.Serializable;
+
+/**
+ * Created by willy on 13.04.14.
+ */
+public class DatabaseConnection implements Serializable {
+    Integer id;
+    String name;
+    String url;
+    String username;
+    String password;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
