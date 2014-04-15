1dc1d29bdfad77337cedb80ab2a83fa822a4ed07
Björn Wilmsmann <bjoern@wilmsmann.de>
2014-04-15 18:55:29 +0200

    REST API

--- src/main/java/com/zenquery/api/QueryVersionController.java
+++ src/main/java/com/zenquery/api/QueryVersionController.java
@@ -29,7 +29,10 @@ public class QueryVersionController {
         return queryVersionDAO.findByQueryId(id);
     }
 
-    @RequestMapping(method = RequestMethod.POST, produces = { "application/xml", "application/json" })
+    @RequestMapping(
+            method = RequestMethod.POST,
+            consumes = { "application/xml", "application/json" },
+            produces = { "application/xml", "application/json" })
     public @ResponseBody
     QueryVersion create(
             @RequestBody QueryVersion queryVersion
@@ -39,7 +42,10 @@ public class QueryVersionController {
         return queryVersionDAO.find(new Long(id.longValue()).intValue());
     }
 
-    @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+    @RequestMapping(
+            value = "/{id}",
+            consumes = { "application/xml", "application/json" },
+            method = RequestMethod.PUT)
     public @ResponseBody String update(
             @PathVariable Integer id,
             @RequestBody QueryVersion queryVersion
