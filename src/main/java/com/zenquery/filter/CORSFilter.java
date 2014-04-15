8acfc6e5151a9f71936e576617feff597e28d128
Björn Wilmsmann <bjoern@wilmsmann.de>
2014-04-15 19:14:24 +0200

    REST API

--- src/main/java/com/zenquery/filter/CORSFilter.java
+++ src/main/java/com/zenquery/filter/CORSFilter.java
@@ -2,14 +2,9 @@ package com.zenquery.filter;
 
 import org.springframework.stereotype.Component;
 
-import java.io.IOException;
-import javax.servlet.Filter;
-import javax.servlet.FilterChain;
-import javax.servlet.FilterConfig;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
+import javax.servlet.*;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 /**
  * Created by willy on 14.04.14.
@@ -20,9 +15,9 @@ public class CORSFilter implements Filter {
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
         HttpServletResponse response = (HttpServletResponse) res;
         response.setHeader("Access-Control-Allow-Origin", "*");
-        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+        response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE");
         response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers", "x-requested-with");
+        response.setHeader("Access-Control-Allow-Headers", "Content-Type, x-requested-with");
         chain.doFilter(req, res);
     }
 
