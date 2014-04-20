6e44cd2a47d7b5da461b621cef9c069cfcaa9a71
Björn Wilmsmann <bjoern@wilmsmann.de>
2014-04-20 14:38:34 +0200

    Heroku

--- src/main/java/com/zenquery/filter/CORSFilter.java
+++ src/main/java/com/zenquery/filter/CORSFilter.java
@@ -12,13 +12,13 @@ import java.io.IOException;
 @Component
 public class CORSFilter implements Filter {
 
-    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
-        HttpServletResponse response = (HttpServletResponse) res;
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain chain) throws IOException, ServletException {
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
         response.setHeader("Access-Control-Allow-Origin", "*");
         response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE");
         response.setHeader("Access-Control-Max-Age", "3600");
         response.setHeader("Access-Control-Allow-Headers", "Content-Type, x-requested-with");
-        chain.doFilter(req, res);
+        chain.doFilter(servletRequest, servletResponse);
     }
 
     public void init(FilterConfig filterConfig) {}
