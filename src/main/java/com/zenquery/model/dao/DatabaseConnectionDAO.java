8c5647fa82cb3aca8c2439f4ec6979da3b600351
Björn Wilmsmann <bjoern@wilmsmann.de>
2014-04-23 22:18:40 +0200

    Caching

--- src/main/java/com/zenquery/model/dao/DatabaseConnectionDAO.java
+++ src/main/java/com/zenquery/model/dao/DatabaseConnectionDAO.java
@@ -1,6 +1,7 @@
 package com.zenquery.model.dao;
 
 import com.zenquery.model.DatabaseConnection;
+import org.springframework.cache.annotation.Cacheable;
 
 import java.util.List;
 
@@ -8,6 +9,7 @@ import java.util.List;
  * Created by willy on 13.04.14.
  */
 public interface DatabaseConnectionDAO {
+    @Cacheable("sql.databaseConnections")
     public DatabaseConnection find(Integer id);
 
     public List<DatabaseConnection> findAll();
