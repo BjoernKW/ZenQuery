e8402f3b14db8a041afd1fe26fd1895537300d3d
Björn Wilmsmann <bjoern@wilmsmann.de>
2014-04-24 10:15:07 +0200

    Caching

--- src/main/java/com/zenquery/model/dao/DatabaseConnectionDAO.java
+++ src/main/java/com/zenquery/model/dao/DatabaseConnectionDAO.java
@@ -1,6 +1,7 @@
 package com.zenquery.model.dao;
 
 import com.zenquery.model.DatabaseConnection;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 
 import java.util.List;
@@ -12,11 +13,28 @@ public interface DatabaseConnectionDAO {
     @Cacheable("sql.databaseConnections")
     public DatabaseConnection find(Integer id);
 
+    @Cacheable("sql.databaseConnections.lists")
     public List<DatabaseConnection> findAll();
 
+    @CacheEvict(
+            value = "sql.databaseConnections.lists",
+            allEntries = true
+    )
     public Number insert(DatabaseConnection databaseConnection);
 
+    @CacheEvict(
+            value = {
+                    "sql.databaseConnections",
+                    "sql.databaseConnections.lists"
+            },
+            allEntries = true)
     public void update(Integer id, DatabaseConnection databaseConnection);
 
+    @CacheEvict(
+            value = {
+                    "sql.databaseConnections",
+                    "sql.databaseConnections.lists"
+            },
+            allEntries = true)
     public void delete(Integer id);
 }
