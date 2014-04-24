1112e8d6d5bf7ccfd49a4b48c505d4fc3805851c
Björn Wilmsmann <bjoern@wilmsmann.de>
2014-04-24 10:15:07 +0200

    Caching

--- src/main/java/com/zenquery/model/dao/QueryVersionDAO.java
+++ src/main/java/com/zenquery/model/dao/QueryVersionDAO.java
@@ -1,6 +1,7 @@
 package com.zenquery.model.dao;
 
 import com.zenquery.model.QueryVersion;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 
 import java.util.List;
@@ -15,19 +16,45 @@ public interface QueryVersionDAO {
     @Cacheable("sql.queryVersions")
     public QueryVersion findByQueryIdAndVersion(Integer id, Integer version);
 
+    @Cacheable("sql.queryVersions")
     public QueryVersion findCurrentByQueryId(Integer id);
 
+    @Cacheable("sql.queryVersions.lists")
     public List<QueryVersion> findByQueryId(Integer id);
 
+    @Cacheable("sql.queryVersions.lists")
     public List<QueryVersion> findPreviousVersionsByQueryId(Integer id);
 
+    @Cacheable("sql.queryVersions.lists")
     public List<QueryVersion> findAll();
 
+    @CacheEvict(
+            value = "sql.queryVersions.lists",
+            allEntries = true
+    )
     public Number insert(QueryVersion QueryVersion);
 
+    @CacheEvict(
+            value = {
+                    "sql.queryVersions",
+                    "sql.queryVersions.lists"
+            },
+            allEntries = true)
     public void update(Integer id, QueryVersion QueryVersion);
 
+    @CacheEvict(
+            value = {
+                    "sql.queryVersions",
+                    "sql.queryVersions.lists"
+            },
+            allEntries = true)
     public void delete(Integer id);
 
+    @CacheEvict(
+            value = {
+                    "sql.queryVersions",
+                    "sql.queryVersions.lists"
+            },
+            allEntries = true)
     public void deleteByQueryId(Integer id);
 }
