eff71f9a2f2829c049a136f0c15499181836f507
Björn Wilmsmann <bjoern@wilmsmann.de>
2014-05-04 22:00:23 +0200

    #10

--- src/main/java/com/zenquery/util/BasicDataSourceFactory.java
+++ src/main/java/com/zenquery/util/BasicDataSourceFactory.java
@@ -2,10 +2,19 @@ package com.zenquery.util;
 
 import org.apache.commons.dbcp2.BasicDataSource;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by willy on 23.04.14.
  */
 public class BasicDataSourceFactory {
+    private Map<String, BasicDataSource> dataSources;
+
+    public BasicDataSourceFactory() {
+        dataSources = new HashMap<String, BasicDataSource>();
+    }
+
     public BasicDataSource getBasicDataSource(
             String driverClassName,
             String url,
@@ -13,14 +22,21 @@ public class BasicDataSourceFactory {
             String password,
             String validationQuery
     ) {
-        BasicDataSource dataSource = new BasicDataSource();
 
-        dataSource.setDriverClassName(driverClassName);
-        dataSource.setUsername(username);
-        dataSource.setPassword(password);
-        dataSource.setUrl(url);
-        dataSource.setMaxIdle(5);
-        dataSource.setValidationQuery(validationQuery);
+        BasicDataSource dataSource = dataSources.get(url);
+
+        if (dataSource == null) {
+            dataSource = new BasicDataSource();
+
+            dataSource.setDriverClassName(driverClassName);
+            dataSource.setUsername(username);
+            dataSource.setPassword(password);
+            dataSource.setUrl(url);
+            dataSource.setMaxIdle(5);
+            dataSource.setValidationQuery(validationQuery);
+
+            dataSources.put(url, dataSource);
+        }
 
         return dataSource;
     }
